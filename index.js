1da867f9150715b810f8fb46731a4b098b3a9796
Areeba Bano <161935706+areebabano@users.noreply.github.com>
2024-03-26 14:10:08 +0500

    CLI based Todo list

---
+++ index.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+let todos = [""];
+let loop = true;
+while (loop) {
+    const answers = await inquirer.prompt([
+        {
+            type: "input",
+            name: "TODO",
+            message: "What do you want to add in your Todo?",
+        },
+        {
+            type: "confirm",
+            name: "AddMore",
+            message: "Do you want to add more Todo?",
+            default: false,
+        },
+    ]);
+    const { TODO, AddMore } = answers;
+    console.log(answers);
+    loop = AddMore;
+    if (TODO) {
+        todos.push(TODO);
+    }
+    else {
+        console.log("Kindly enter valid input");
+    }
+    console.log("Your Todo List:");
+    if (todos.length > 0) {
+        todos.forEach((todo) => {
+            console.log(todo);
+        });
+    }
+    else {
+        console.log("Todos Not Found!");
+    }
+}
